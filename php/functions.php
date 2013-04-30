f0e02a4a613fe3e6d057db011f7809fcc0bce466
Alex Jiang <alexjiang02@gmail.com>
2013-04-30 15:44:47 +0800

    Add Web UI

---
+++ php/functions.php
@@ -0,0 +1,147 @@
+<?php
+
+function _get_summary($field_name, $hd)
+{
+  $line = fgets($hd);
+
+  $data = array();
+  preg_match("/^$field_name = (.*)/", $line, $data)
+  or die("Invalid summary file: " . $line);
+
+  return split(',', $data[1]);
+}
+
+function get_summary()
+{
+  $hd = fopen(SUMMARY_FILE, 'r')
+    or die("Cannot open summary file: " . SUMMARY_FILE);
+
+  $avg_score = _get_summary('AVG SCORE', $hd);
+  $avg_delta = _get_summary('AVG DELTA', $hd);
+
+  fclose($hd);
+
+  return array($avg_score, $avg_delta);
+}
+
+function get_info_files()
+{
+  foreach (glob(INFO_DIR . "/*") as $filename)
+  {
+    if (!$first || $filename > $first)
+    {
+      $second = $first;
+      $first = $filename;
+    }
+    else if (!$second || $filename > $second)
+    {
+      $second = $filename;
+    }
+  }
+
+  return array($first, $second);
+}
+
+function calc_score($reposts_count, $comments_count)
+{
+  return $reposts_count + 2 * $comments_count;
+}
+
+function _get_posts($info_file)
+{
+  $hd = fopen($info_file, 'r')
+    or die("Cannot open info file: " . $info_file);
+  $posts = array();
+  
+  while ($line = fgets($hd))
+  {
+    $line = trim($line);
+    list($id,$reposts_count,$comments_count,$datetime,$epoch,$age)
+      = split(',', $line);
+    $posts[$id] = array($age, calc_score($reposts_count, $comments_count));
+  }
+  fclose($hd);
+  
+  return $posts;
+}
+
+function get_posts($first, $second)
+{
+  $old_posts = _get_posts($second);
+  $posts = _get_posts($first);
+  
+  // Calc delta
+  foreach ($posts as $id => $data)
+  {
+    if (isset($old_posts[$id]))
+    {
+      $old_score = $old_posts[$id][1];
+      $score = $data[1];
+      $delta = $score - $old_score;
+      array_push($posts[$id], $delta);
+    }
+    else
+    {
+      array_push($posts[$id], 0);
+    }
+  }
+
+  return $posts;
+}
+
+function update_final_score($posts, $avg_scores, $avg_deltas)
+{
+  $new_posts = array();
+  foreach ($posts as $id => $data)
+  {
+    list($age, $score, $delta) = $data;
+    if (isset($avg_scores[$age]) && isset($avg_deltas[$age]))
+    {
+      $avg_score = $avg_scores[$age];
+      $avg_delta = $avg_deltas[$age];
+      $new_posts[$id] = array($age, $score, $delta,
+                      $score - $avg_score, $delta - $avg_delta);
+    }
+  }
+  return $new_posts;
+}
+
+function _get_text($id)
+{
+  $ch = curl_init();
+  
+  curl_setopt($ch,CURLOPT_URL, "https://api.weibo.com/2/statuses/show.json?access_token=" . ACCESS_TOKEN . "&id=" . $id);
+  curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, FALSE);
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER, TRUE);
+  $response = curl_exec($ch);
+  $error = curl_error($ch);
+  
+  curl_close($ch);
+  
+  if ($error)
+  {
+    die("Failed to fetch text for " . $id . ", error: $error");
+  }
+  else
+  {
+    $post = json_decode($response, true);
+    return $post['text'];
+  }  
+}
+
+function get_text($id)
+{
+  $cache = TXT_DIR . "/$id";
+  if (file_exists($cache))
+  {
+    return file_get_contents($cache);
+  }
+  else
+  {
+    $txt = _get_text($id);
+    file_put_contents($cache, $txt);
+    return $txt;
+  }
+}
+
+?>
---
+++ php/index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Weibo News / 新闻聚光灯</title>
+<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+<style type="text/css">
+.post {
+  border-bottom: 1px grey dotted;
+  margin-bottom: 1em;
+  overflow: hidden;
+  height: 120px;
+}
+h1 {
+  font-family: Arial, "Microsoft YaHei", sans-serif;
+  font-size: 24px;
+  text-align: center;
+  border-bottom: 1px grey dotted;
+}
+.txt {
+  font-family: "Consolas", "Simsun", sans-serif;
+  font-size: 12px;
+}
+.info {
+  float: left;
+  width: 60px;
+  height: 100%;
+  text-align: center;
+  font-family: "Consolas", sans-serif;
+  font-size: 12px;
+  padding-top: 20px;
+}
+.debug {
+  font-family: "Consolas", sans-serif;
+  font-size: 9px;
+  color: silver;
+}
+</style>
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="span3">&nbsp;</div>
+      <div class="span6">
+        <h1>Weibo News / 新闻聚光灯</h1>
+      </div>
+      <div class="span3">&nbsp;</div>
+    </div>
+<?php
+
+require_once('config.php');
+require_once('functions.php');
+
+
+list($avg_score, $avg_delta) = get_summary();
+
+
+list($first, $second) = get_info_files();
+
+
+$posts = get_posts($first, $second);
+$posts = update_final_score($posts, $avg_score, $avg_delta);
+
+{
+  $score = array();
+  foreach ($posts as $id => $data)
+  {
+    $score[$id] = $data[3];
+  }
+  array_multisort($score, SORT_DESC, $posts);
+}
+
+$i = 0;
+foreach ($posts as $id => $data)
+{
+  list($age,$total_score,$total_delta,$score,$delta) = $data;
+  ?>
+  <div class="row">
+    <div class="span3">&nbsp;</div>
+    <div class="span6">
+      <div class="post">
+        <div class="info">
+          <div class="delta"><?php echo $delta > 0 ? '<img src="images/arrow-alt-up.png"></img>' : '<img src="images/arrow-alt-down.png"></img>'?></div>
+          <div class="score"><?php echo $score?></div>
+        </div>
+        <div class="txt">
+          <?php echo get_text($id)?>
+        </div>
+        <div class="debug">
+        AGE:<?php echo $age?>
+        TS:<?php echo $total_score?>
+        TD:<?php echo $total_delta?>
+        S:<?php echo $score?>
+        D:<?php echo $delta?>
+        </div>
+      </div>
+    </div>
+    <div class="span3">&nbsp;</div>
+  </div>
+<?php   
+  ++$i;
+  if ($i >= POST_COUNT)
+  {
+    break;
+  }
+}
+
+?>
+</div>
+</body>
+<?php 
+  // Debug output
+  print "<!-- AVG SCORE = " . join(',', $avg_score) . " -->\n";
+  print "<!-- AVG DELTA = " . join(',', $avg_delta) . " -->\n";
+  print "<!-- FST = $first -->\n";
+  print "<!-- SND = $second -->\n";
+  print "<!--\n";
+  foreach ($posts as $id => $data)
+  {
+    print "  $id," . join(',', $data) . "\n";
+  }
+  print "-->\n";
+?>
+</html>
