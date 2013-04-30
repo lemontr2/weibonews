28fc10f0cb2ede94bb2590c6a5b8b5edfc642baa
Alex Jiang <alexjiang02@gmail.com>
2013-04-30 10:33:21 +0800

    Update scrape.pl to take access token as an argument

--- scrape.pl
+++ scrape.pl
@@ -17,46 +17,64 @@ sub err_msg($)
   warn "Failed to fetch " . $res->request()->uri . ": " . $res->status_line;
 }
 
-my $ua = LWP::UserAgent->new();
-
-my $strp = DateTime::Format::Strptime->new(
-  pattern => '%a %b %d %T %z %Y',
-  on_error => 'croak',
-);
-my $now = time();
-
-my $uid = 1618051664; # breakingnews
-my $access_token = '';
-my $count = 100;
-
-my $url = url('https://api.weibo.com/2/statuses/user_timeline.json');
-
-$url->query_form(
-  access_token    => $access_token,
-  uid       => $uid,
-  count     => $count,
-  page      => 1,
-  trim_user => 1,
-  feature   => 1,
-);
-
-my $response = $ua->get($url);
-if ($response->is_success())
+sub scrape($)
 {
-  my $json = $response->decoded_content();
-  my $posts = decode_json( $json );
-  for (@{ $posts->{statuses} })
+  my ($access_token) = @_;
+  my $ua = LWP::UserAgent->new();
+  
+  my $strp = DateTime::Format::Strptime->new(
+    pattern => '%a %b %d %T %z %Y',
+    on_error => 'croak',
+  );
+  my $now = time();
+  
+  my $uid = 1618051664; # breakingnews
+  my $count = 100;
+  
+  my $url = url('https://api.weibo.com/2/statuses/user_timeline.json');
+  
+  $url->query_form(
+    access_token    => $access_token,
+    uid       => $uid,
+    count     => $count,
+    page      => 1,
+    trim_user => 1,
+    feature   => 1,
+  );
+  
+  my $response = $ua->get($url);
+  if ($response->is_success())
   {
-    # Parse datetime
-    my $created_at = $_->{created_at};
-    my $datetime = $strp->parse_datetime($created_at);
-    my $epoch = $datetime->epoch;
-    my $age = int( ( $now - $epoch ) / 3600 );
-
-    print join(',', @{$_}{qw/id reposts_count comments_count/}, $datetime, $epoch, $age), "\n";
+    my $json = $response->decoded_content();
+    my $posts = decode_json( $json );
+    for (@{ $posts->{statuses} })
+    {
+      # Parse datetime
+      my $created_at = $_->{created_at};
+      my $datetime = $strp->parse_datetime($created_at);
+      my $epoch = $datetime->epoch;
+      my $age = int( ( $now - $epoch ) / 3600 );
+  
+      print join(',', @{$_}{qw/id reposts_count comments_count/}, $datetime, $epoch, $age), "\n";
+    }
+  }
+  else
+  {
+    err_msg($response);
   }
 }
-else
+
+sub main
 {
-  err_msg($response);
+  my ($access_token) = @_;
+  if ($access_token)
+  {
+    scrape($access_token);
+  }
+  else
+  {
+    print "Usage: $0 access_token\n";
+    exit 0;
+  }
 }
+main(@ARGV);
